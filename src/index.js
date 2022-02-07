8c0b4f178bb285668d75c019efaac4d56fe52b01
Natawik-B <natawwwik37@gmail.com>
2022-02-07 20:59:07 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function toReadable (number) {
-  
-}
+    let num1 = 'zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen'.split(" ");
+    let num2 = 'twenty thirty forty fifty sixty seventy eighty ninety'.split(" ");
+    let rank = number%10;
+
+    if (number < 20) {
+        return num1[number];
+    } else if (number < 100) {
+        return num2[Math.floor(number/10)-2] + (rank? " " + num1[rank]: "");
+    } else if (number < 1000) {
+        return num1[Math.floor(number/100)] + " hundred" + (number%100 == 0? "": " " + toReadable(number%100));
+    } else {
+        return toReadable(Math.floor(number/1000)) + " thousand" + (number%1000 != 0? " " + toReadable(number%1000): "");
+    }
+};
